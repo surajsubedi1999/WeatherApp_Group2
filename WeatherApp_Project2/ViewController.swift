78e5b9b137d38a9278f8e52aadcad1192a111323
Suraj Subedi <surajsubedi@Surajs-MacBook-Pro.local>
2025-07-25 18:38:03 -0400

    First Commit UI added

---
+++ WeatherApp_Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_Project2
+//
+//  Created by Suraj Subedi on 2025-07-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
